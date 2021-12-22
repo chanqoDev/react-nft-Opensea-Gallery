9d777d98464c37437a0cb0cf43a9edc0853f28d0
Christian E <christianelena92@gmail.com>
2021-12-22 12:36:56 -0600

    add NFT img assets adn create collection Card and add styling

--- react-nft-challenge/src/App.js
+++ react-nft-challenge/src/App.js
@@ -1,11 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import CollectionCard from "./components/CollectionCard";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+        id={0}
+        name={"kikko punk"}
+        traits={[{ value: 8 }]}
+        image="https://lh3.googleusercontent.com/zOmQUbs9DhiXNi7U9aWWzrmfTfIdH-oIRETrLyKSr7_1huclb28J_TUaAtcIvVBQSllvXJA13PxJmtPtwU05_Ozt1FsGyWynHcJiSA=w600"
+      />
     </div>
   );
 }
