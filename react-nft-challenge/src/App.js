499d9a4bf59a185dffac8002d690c26c96747d22
Christian E <christianelena92@gmail.com>
2021-12-29 21:48:31 -0600

    fetch image and listen to changes and display click punk.id

--- react-nft-challenge/src/App.js
+++ react-nft-challenge/src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 // import CollectionCard from "./components/CollectionCard";
--- react-nft-challenge/src/components/PunkList.jsx
+++ react-nft-challenge/src/components/PunkList.jsx
@@ -6,7 +6,7 @@ const PunkList = ({ punkListData }) => {
   return (
     <div className="punkList">
       {punkListData.map((punk) => (
-        <div>
+        <div onClick={() => console.log(punk.token_id)}>
           <CollectionCard
             key={punk.token_id}
             id={punk.token_id}
